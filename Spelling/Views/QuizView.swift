cf6850aa43681f60ac97daae408ee1e275097bc7
Griffin Muir <gmuir@lcs.on.ca>
2023-10-30 09:03:30 -0400

    made a text field and if else stament

--- Spelling/Views/QuizView.swift
+++ Spelling/Views/QuizView.swift
@@ -8,19 +8,42 @@
 import SwiftUI
 
 struct QuizView: View {
+    enum Outcome: String {
+        
+    }
     
     // MARK: Stored properties
     @State var currentItem = itemsToSpell.randomElement()!
-    
+    @State var userGuess = ""
+    @State var awnser = ""
     // MARK: Computed properties
     var body: some View {
         
         VStack {
+            Text("\(awnser)")
+            
             Image(currentItem.imageName)
                 .resizable()
                 .scaledToFit()
+            HStack {
+                TextField("enter word here", text: $userGuess)
+                (Button(action: {
+                    checkGuess()
+                }, label: {
+                    Text("Button")
+                }))
+            }
+            .padding()
         }
-        
+    }
+    func checkGuess() {
+        if userGuess == currentItem.word{
+            awnser = "correct"
+        userGuess = ""
+        } else {
+           awnser = "wrong"
+        }
+            
     }
 }
 
